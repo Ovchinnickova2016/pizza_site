736e9356205bf57f34a1c50b0d746233fb3cb967
anonimD08 <asdebug@yandex.ru>
2018-11-27 10:13:31 +0500

    url

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,15 +1,12 @@
 from django.urls import path ,re_path
-from .views import index, login, register, contacts, delivery, vacations
+from .views import ProductList, ProductDetail
 """Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 """
 
 urlpatterns = [
-    re_path(r'^$',index, name='index'),
-    re_path(r'^login', login, name='login'),
-    re_path(r'^register', register, name='register'),
-    re_path(r'^contacts', contacts, name='contacts'),
-    re_path(r'^delivery', delivery, name='delivery'),
-    re_path(r'^vacations', vacations, name='vacations'),
+    re_path(r'^$',ProductList, name='ProductList'),
+    re_path(r'^(?P<category_slug>[-\w]+)/$', ProductList, name='ProductListByCategory'),
+    re_path(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', ProductDetail, name='ProductDetail'),
 ]
